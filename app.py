2b26da216bee04d3abe3f755fbf900ac9df7bd2a
wibbler67 <benjhgould@gmail.com>
2022-10-08 21:00:14 +0100

    try this

--- app.py
+++ app.py
@@ -3,61 +3,60 @@ import os
 from flask import Flask
 from werkzeug.middleware.proxy_fix import ProxyFix
 
+test_config = None
 
-def create_app(test_config=None):
-    app = Flask(__name__, instance_relative_config=True)
-    app.config.from_mapping(
-        SECRET_KEY='dev',
-        DATABASE=os.path.join(app.instance_path, 'flask.sqlite'),
-    )
+app = Flask(__name__, instance_relative_config=True)
+app.config.from_mapping(
+    SECRET_KEY='dev',
+    DATABASE=os.path.join(app.instance_path, 'flask.sqlite'),
+)
 
-    if test_config is None:
-        app.config.from_pyfile('config.py', silent=True)
-    else:
-        app.config.from_mapping(test_config)
+if test_config is None:
+    app.config.from_pyfile('config.py', silent=True)
+else:
+    app.config.from_mapping(test_config)
 
-    try:
-        os.mkdir(app.instance_path)
-    except OSError:
-        pass
+try:
+    os.mkdir(app.instance_path)
+except OSError:
+    pass
 
-    from app_blueprint import db
-    db.init_app(app)
+from app_blueprint import db
+db.init_app(app)
 
-    from app_blueprint import auth
-    app.register_blueprint(auth.bp)
+from app_blueprint import auth
+app.register_blueprint(auth.bp)
 
-    from app_blueprint import fixtures
-    app.register_blueprint(fixtures.bp)
+from app_blueprint import fixtures
+app.register_blueprint(fixtures.bp)
 
-    from app_blueprint import availability
-    app.register_blueprint(availability.bp)
+from app_blueprint import availability
+app.register_blueprint(availability.bp)
 
-    from app_blueprint import account
-    app.register_blueprint(account.bp)
+from app_blueprint import account
+app.register_blueprint(account.bp)
 
-    from app_blueprint import results
-    app.register_blueprint(results.bp)
+from app_blueprint import results
+app.register_blueprint(results.bp)
 
-    from app_blueprint import posts
-    app.register_blueprint(posts.bp)
+from app_blueprint import posts
+app.register_blueprint(posts.bp)
 
-    from app_blueprint import home
-    app.register_blueprint(home.bp)
-    app.add_url_rule('/', endpoint='index')
+from app_blueprint import home
+app.register_blueprint(home.bp)
+app.add_url_rule('/', endpoint='index')
 
-    from app_blueprint import subs
-    app.register_blueprint(subs.bp)
+from app_blueprint import subs
+app.register_blueprint(subs.bp)
 
-    from app_blueprint import admin
-    app.register_blueprint(admin.bp)
+from app_blueprint import admin
+app.register_blueprint(admin.bp)
 
-    app.jinja_env.globals.update(get_result=results.get_result)
+app.jinja_env.globals.update(get_result=results.get_result)
 
-    app.wsgi_app = ProxyFix(
-        app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_prefix=1
-    )
-    return app
+app.wsgi_app = ProxyFix(
+    app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_prefix=1
+)
 
-
-create_app()
+if __name__ == '__main__':
+    app.run("0.0.0.0")
---
+++ db_init.py
@@ -0,0 +1,3 @@
+from app import app
+
+app.init_db()
